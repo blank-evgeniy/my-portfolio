c4041abd7acd5ea0b543d66d58c024a70bd60dc7
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-07-01 22:06:28 +0200

    Added skills section

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { SiSass } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaReact } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+
+
+
 import './Style.scss';
 
 const Skills: React.FC  = () => {
@@ -7,9 +18,14 @@ const Skills: React.FC  = () => {
         <section className='skills'>
             <h2 className='skills__title title'>Мои навыки</h2>
             <div className='skills__list'>
-                <img className='skills__item' alt='html'></img>
-                <img className='skills__item' alt='css'></img>
-                <img className='skills__item' alt='js'></img>
+                <FaHtml5 size={'128px'} className='skills__item'/>
+                <FaCss3Alt size={'128px'} className='skills__item'/>
+                <SiJavascript size={'128px'} className='skills__item'/>
+                <SiTypescript size={'128px'} className='skills__item'/>
+                <SiSass size={'128px'} className='skills__item'/>
+                <RiTailwindCssFill size={'128px'} className='skills__item'/>
+                <FaReact size={'128px'} className='skills__item'/>
+                <SiRedux size={'128px'} className='skills__item'/>
             </div>
         </section>
     );

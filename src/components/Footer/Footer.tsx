8f219f8e1057ec74d8a610e40088e209ec080220
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-07-01 22:19:52 +0200

    Added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import { FaGithub } from "react-icons/fa";
+
 import './Style.scss';
 
 const Footer: React.FC  = () => {
     return (
         <footer className='footer'>
-            <div className='footer__list'>
-                <a className='footer__link'><img alt='GitHub'></img></a>
-            </div>
+            <a className='footer__link' href='https://github.com/blank-evgeniy'><FaGithub size={'56px'}/></a>
+            <p className='footer__text'>Copyright © Eugene Blank</p>
         </footer>
     );
 };

98f32ecce17cb26382af0570ff1ff53e0f70719f
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-07-01 19:47:46 +0200

    Added style for header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header"
 import Projects from "./components/Projects/Projects"
 import Skills from "./components/Skills/Skills"
 
+import './styles/global.scss'
 
 function App() {
   return (
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import Logo from './../../images/github-logo.png';
 import './Style.scss';
 
 const Header: React.FC  = () => {
     return (
         <header className='header'>
-            <a className='header__git-link'>GitHub</a>
-            <p className='header__name'>Евгений Бланк</p>
-            <p className='header__profession'>frontend developer</p>
+            <a className='header__git-link' href='https://github.com/blank-evgeniy/my-portfolio'>
+                <img className='header__git-logo' src={Logo}/>
+            </a>
+            <div className='container'>
+                <p className='header__name'>Евгений Бланк</p>
+                <p className='header__profession'>frontend developer</p>
+            </div>
+
         </header>
     );
 };
